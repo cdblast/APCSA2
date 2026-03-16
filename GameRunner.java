954bb2d5917afcfe296b00a354524af0392d14a1
theriobyte <719044@student.dusd.net>
2026-03-15 17:14:28 -0700

    added replaying

--- GameRunner.java
+++ GameRunner.java
@@ -3,15 +3,45 @@
  *
  * GameRunner Class for the Game of Nim 
 */
+import java.util.Scanner;
+
 public class GameRunner
 {
   public static void main(String[] args)
   {
+      boolean playing = true;
+      String response = null;
+      Scanner input = new Scanner(System.in);
+
     System.out.println("Welcome to the Game of Nim!");
 
-    Board.populate(); 
+    while (playing){
+      Board.populate(); 
   
-    Game nim = new Game(); 
-    nim.play();
+      Game nim = new Game(); 
+      nim.play(); 
+      
+      playing = false;
+
+      while (response == null){
+        System.out.println("Do you want to play again? (Y/N) ");
+        response = input.nextLine();
+        System.out.println(response);
+
+        if((response.equals("Y")) || response.equals("y")){
+          playing = true;
+        }
+        else if((response.equals("N")) || response.equals("n")){
+          playing = false;
+        }
+        else{
+          System.out.println("Your response was invalid.");
+          response = null;
+        }
+      
+      }
+      response = null;
+    }
+
   }
 }
